140553f92ff311b52f1f6567826ed07a6fbf7e8f
AnKyungMoo <sodp2@naver.com>
2022-07-17 00:00:29 +0900

    편의점 정보 가져오는 api 추가

---
+++ app/src/main/java/com/km/real_convenience_store/dto/ProductDTO.kt
@@ -0,0 +1,25 @@
+package com.km.real_convenience_store.dto
+
+import com.google.gson.annotations.SerializedName
+
+data class ProductsResponseBody(
+    @SerializedName("page_data") val pageData: PageDataDTO,
+    @SerializedName("data") val data: List<ProductDTO>
+)
+
+data class PageDataDTO(
+    @SerializedName("count") val count: Int?,
+    @SerializedName("next") val next: String?,
+    @SerializedName("size") val size: Int?,
+    @SerializedName("previous") val previous: String?,
+)
+
+data class ProductDTO(
+    @SerializedName("id") val id: Int?,
+    @SerializedName("store") val store: String?,
+    @SerializedName("title") val title: String?,
+    @SerializedName("price") val price: Int?,
+    @SerializedName("sale_type") val saleType: String?,
+    @SerializedName("image") val image: String?,
+    @SerializedName("created_at") val createdAt: String?,
+)
---
+++ app/src/main/java/com/km/real_convenience_store/network/service/ConvenienceStoreService.kt
@@ -0,0 +1,18 @@
+package com.km.real_convenience_store.network.service
+
+import com.km.real_convenience_store.dto.ProductsResponseBody
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface ConvenienceStoreService {
+    @GET("api/conveniences/products/")
+    suspend fun getProducts(
+        @Query("month") month: String? = null,
+        @Query("page") page: Int? = null,
+        @Query("sale_type") saleType: String? = null,
+        @Query("size") size: Int? = null,
+        @Query("store") store: String? = null,
+        @Query("title") title: String? = null,
+        @Query("year") year: String? = null,
+    ): ProductsResponseBody
+}
