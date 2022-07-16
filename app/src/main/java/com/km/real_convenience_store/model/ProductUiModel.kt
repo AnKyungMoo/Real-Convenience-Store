578968ede935e98d3b22ea3151291e7257935457
AnKyungMoo <sodp2@naver.com>
2022-07-17 02:05:39 +0900

    상품 검색 ui

---
+++ app/src/main/java/com/km/real_convenience_store/model/ProductUiModel.kt
@@ -0,0 +1,9 @@
+package com.km.real_convenience_store.model
+
+data class ProductUiModel(
+    val storeName: String,
+    val eventPeriod: String,
+    val productImageUrl: String,
+    val description: String,
+    val price: String,
+)
