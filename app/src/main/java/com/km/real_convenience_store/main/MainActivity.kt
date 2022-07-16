e43b7cbdb0ebeeb432b01582c7f818c57efa4ada
hr955 <nfzoze01@gmail.com>
2022-07-17 05:58:35 +0900

    favoriteProductAdapter 수정

--- app/src/main/java/com/km/real_convenience_store/main/MainActivity.kt
+++ app/src/main/java/com/km/real_convenience_store/main/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.km.real_convenience_store.main
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
@@ -7,9 +8,11 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import com.km.real_convenience_store.R
 import com.km.real_convenience_store.database.AppDatabase
 import com.km.real_convenience_store.databinding.ActivityMainBinding
-import com.km.real_convenience_store.dto.local.FavoriteProductEntity
-import com.km.real_convenience_store.main.adapter.FavoriteProductListAdapter
+import com.km.real_convenience_store.main.adapter.FavoriteProductAdapter
 import com.km.real_convenience_store.main.adapter.FavoriteProductListDecorator
+import com.km.real_convenience_store.model.ProductUiModel
+import com.km.real_convenience_store.ui.product_search.ProductSearchActivity
+import com.km.real_convenience_store.ui.product_search.adapter.ProductSearchAdapter
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
@@ -17,25 +20,39 @@ import kotlinx.coroutines.launch
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
-    private val favoriteProductListAdapter = FavoriteProductListAdapter()
+    private val favoriteProductListAdapter = FavoriteProductAdapter()
+    private val productSearchAdapter = ProductSearchAdapter()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
+        moveToProductSearch()
+        connectFavoriteProductAdapter()
+
+    }
+
+    private fun connectFavoriteProductAdapter() {
         CoroutineScope(Dispatchers.IO).launch {
-            val favoriteProducts = AppDatabase.getInstance(this@MainActivity).userInfoDAO().getFavoriteProduct()
+            val favoriteProducts =
+                AppDatabase.getInstance(this@MainActivity).userInfoDAO().getFavoriteProduct()
             favoriteProductListAdapter.addFavoriteProducts(favoriteProducts)
         }
 
         binding.rvFavoriteProduct.apply {
-            adapter = FavoriteProductListAdapter()
+            adapter = favoriteProductListAdapter
             layoutManager = LinearLayoutManager(
                 this@MainActivity,
                 LinearLayoutManager.HORIZONTAL,
                 false
             )
             addItemDecoration(FavoriteProductListDecorator())
+        }
+    }
+
+    private fun moveToProductSearch() {
+        binding.tvInputHint.setOnClickListener {
+            startActivity(Intent(this, ProductSearchActivity::class.java))
         }
     }
 }
