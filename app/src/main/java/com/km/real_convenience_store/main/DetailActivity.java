6039dbf48fac2e6bebba8142a4d4ce28100c8ca0
leewoongi <t2korea@naver.com>
2022-07-17 03:51:00 +0900

    detail 화면 xml 스크롤뷰 추가 왜 돌아가는거지??

--- app/src/main/java/com/km/real_convenience_store/main/DetailActivity.java
+++
@@ -1,5 +0,0 @@
-package com.km.real_convenience_store.main;
-
-public class DetailActivity {
-
-}
---
+++ app/src/main/java/com/km/real_convenience_store/main/DetailActivity.kt
@@ -0,0 +1,13 @@
+package com.km.real_convenience_store.main
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.km.real_convenience_store.R
+
+class DetailActivity: AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail)
+    }
+}
