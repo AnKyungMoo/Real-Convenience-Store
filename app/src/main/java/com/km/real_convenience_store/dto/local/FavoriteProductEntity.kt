3a9ff13c4de6e7c2e6a2a07d306c04a5ba7961fb
hr955 <nfzoze01@gmail.com>
2022-07-17 01:14:08 +0900

    즐겨찾기 테이블 이름 추가

--- app/src/main/java/com/km/real_convenience_store/dto/local/FavoriteProductEntity.kt
+++ app/src/main/java/com/km/real_convenience_store/dto/local/FavoriteProductEntity.kt
@@ -3,7 +3,7 @@ package com.km.real_convenience_store.dto.local
 import androidx.room.ColumnInfo
 import androidx.room.Entity
 
-@Entity(primaryKeys = ["title", "store"])
+@Entity(primaryKeys = ["title", "store"], tableName = "favorite_product")
 data class FavoriteProductEntity(
     @ColumnInfo(name = "title") val title: String,
     @ColumnInfo(name = "store") val store: String,
