bb385bf164af54951a9d45b5c51ff58591a21f74
hr955 <nfzoze01@gmail.com>
2022-07-17 01:07:11 +0900

    테스트 코드 삭제

--- app/src/main/java/com/km/real_convenience_store/main/MainActivity.kt
+++ app/src/main/java/com/km/real_convenience_store/main/MainActivity.kt
@@ -1,25 +1,13 @@
-package com.km.real_convenience_store
+package com.km.real_convenience_store.main
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.km.real_convenience_store.database.AppDatabase
-import com.km.real_convenience_store.dto.local.FavoriteProductEntity
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
+import androidx.appcompat.app.AppCompatActivity
+import com.km.real_convenience_store.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        CoroutineScope(Dispatchers.Default).launch {
-            AppDatabase.getInstance(this@MainActivity).userInfoDAO().addFavoriteProduct(
-                FavoriteProductEntity(
-                    title = "콜라",
-                    store = "cu"
-                )
-            )
-        }
     }
 }
